7b88319d52b8b5c0bf1da56587fe6afed61ace71
ramyaagowri <ramyaagowrik@gmail.com>
2025-07-07 20:45:04 +0530

    version 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="text-green-500 bg-pink-500 underline p-5">hiii</div>;
+  return (
+    <div className="text-green-500 bg-pink-500 underline p-5">
+      {console.log("Heyyy Version 1")}
+      <span>Hiiii</span>
+    </div>
+  );
 }
 
 export default App;
