4278c5714d2c634f10d8fdd84415be0e74011d1b
ramyaagowri <ramyaagowrik@gmail.com>
2025-07-08 11:20:22 +0530

    changes made from verion1

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="text-green-500 bg-pink-500 underline p-5">
       {console.log("checking if remote and local sync")}
+      {console.log(
+        "Im making changes to the version 1 branch and commiting it"
+      )}
       <span>Hiiii</span>
     </div>
   );
