4404f17f3ebd8cab88c26645097cb49fbe2547a3
ramyaagowri <ramyaagowrik@gmail.com>
2025-07-08 10:19:52 +0530

    Check git sync

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="text-green-500 bg-pink-500 underline p-5">
-      {console.log("Heyyy Version 1")}
-      {console.log("Heyy version 2")}
+      {console.log("checking if remote and local sync")}
       <span>Hiiii</span>
     </div>
   );
