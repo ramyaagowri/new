27a602dee7d6337ba661a1292c09c841a3b51f0f
ramyaagowri <ramyaagowrik@gmail.com>
2025-07-08 11:23:05 +0530

    2 changes from verio1

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <div className="text-green-500 bg-pink-500 underline p-5">
       {console.log("checking if remote and local sync")}
       {console.log(
-        "Im making changes to the version 1 branch and commiting it"
+        "Im making changes to the version 1 branch and commiting it again"
       )}
       <span>Hiiii</span>
     </div>
